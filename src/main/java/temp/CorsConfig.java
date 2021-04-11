26f0c8091b33e5ccfe2bc1b25e9992935bacdbef
westonk21 <42201255+westonk21@users.noreply.github.com>
2021-04-11 22:12:13 +0700

    Update CorsConfig.java

--- src/main/java/temp/CorsConfig.java
+++ src/main/java/temp/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 				registry.addMapping("/**")
 					.allowedMethods("GET", "POST", "PUT", "DELETE")
 					.allowedHeaders("*")
-					.allowedOrigins("http://localhost");
+					.allowedOrigins("https://pasar-yuk.herokuapp.com");
 			}
 		};
 	}
