d887d1504a4266a660fa9328a030d494da3d44b9
westonk21 <42201255+westonk21@users.noreply.github.com>
2021-04-11 23:44:45 +0700

    Update BuyerController.java

--- src/main/java/com/example/pasarYuk/controller/BuyerController.java
+++ src/main/java/com/example/pasarYuk/controller/BuyerController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import com.example.pasarYuk.model.Buyer;
 //import com.example.pasarYuk.repository.BuyerRepository;
 import com.example.pasarYuk.services.BuyerService;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 
