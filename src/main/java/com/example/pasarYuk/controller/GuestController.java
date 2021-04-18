39fa4d793af9ba18559aaccce552140e41acea2a
westonk21 <westonkierangen21@gmail.com>
2021-04-18 14:18:51 +0700

    update origin2

--- src/main/java/com/example/pasarYuk/controller/GuestController.java
+++ src/main/java/com/example/pasarYuk/controller/GuestController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.example.pasarYuk.model.Guest;
 import com.example.pasarYuk.repository.GuestRepository;
 import com.example.pasarYuk.services.GuestService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1")
 public class GuestController {
